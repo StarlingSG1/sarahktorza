ea15d60e2467a6850b00be4ac82672da6af6f9e4
Jérémie <barriere.jeremie@gmail.com>
2023-09-06 11:32:10 +0200

    [add] header icons

--- app/components/Frise.tsx
+++ app/components/Frise.tsx
@@ -15,15 +15,7 @@ export default function Frise() {
       title: "Baccalaureat STMG",
       description:
         "J’ai obtenue mon Bac STMG option mercatique au lycée Sophie Barat à Châtenay-Malabry dans le 92",
-      style: `transition-all duration-500 opacity-${showDiv ? "100" : "0"}`,
-    },
-    {
-      img: "",
-      year: "2019",
-      title: "Baccalaureat STMG",
-      description:
-        "J’ai obtenue mon Bac STMG option mercatique au lycée Sophie Barat à Châtenay-Malabry dans le 92",
-      style: `transition-all duration-500 delay-500 opacity-${
+      style: `transition-all duration-500 sm:w-auto w-[350px] opacity-${
         showDiv ? "100" : "0"
       }`,
     },
@@ -33,14 +25,27 @@ export default function Frise() {
       title: "Baccalaureat STMG",
       description:
         "J’ai obtenue mon Bac STMG option mercatique au lycée Sophie Barat à Châtenay-Malabry dans le 92",
-      style: `transition-all duration-500 delay-1000 opacity-${
+      style: `transition-all duration-500 sm:w-auto w-[350px] delay-500 opacity-${
+        showDiv ? "100" : "0"
+      }`,
+    },
+    {
+      img: "",
+      year: "2019",
+      title: "Baccalaureat STMG",
+      description:
+        "J’ai obtenue mon Bac STMG option mercatique au lycée Sophie Barat à Châtenay-Malabry dans le 92",
+      style: `transition-all duration-500 sm:w-auto w-[350px] delay-1000 opacity-${
         showDiv ? "100" : "0"
       }`,
     },
   ];
 
   return (
-    <div className="py-20" id="formations">
+    <div
+      className="py-20"
+      id="formations"
+    >
       <div className="max-w-screen-xl mx-auto px-4 md:px-8">
         <h2 className="text-3xl text-gray-800 font-semibold text-center">
           MES FORMATIONS
@@ -50,18 +55,20 @@ export default function Frise() {
             showDiv ? "w-screen" : "w-0"
           } absolute left-0 translate-y-[175%] h-1.5 bg-primary mt-16`}
         ></div>
-        <div className="w-full grid grid-cols-3 gap-6 mt-16">
-          {data.map((item, index) => (
-            <div
-              key={index}
-              className={item.style}
-            >
-              <div className="rounded-full w-7 h-7 bg-primary mb-5"></div>
-              <h3 className="text-2xl font-black">{item.year}</h3>
-              <p className="font-medium text-lg">{item.title}</p>
-              <p>{item.description}</p>
-            </div>
-          ))}
+        <div className="w-full overflow-x-auto">
+          <div className="w-[1000px] sm:w-full flex sm:grid sm:grid-cols-3 gap-6 mt-16">
+            {data.map((item, index) => (
+              <div
+                key={index}
+                className={item.style}
+              >
+                <div className="rounded-full w-7 h-7 bg-primary mb-5"></div>
+                <h3 className="text-2xl font-black">{item.year}</h3>
+                <p className="font-medium text-lg">{item.title}</p>
+                <p>{item.description}</p>
+              </div>
+            ))}
+          </div>
         </div>
       </div>
     </div>
