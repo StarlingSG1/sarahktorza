3f763b9560b46a280d2097864f31f26865c4105f
Jérémie <barriere.jeremie@gmail.com>
2023-09-06 14:42:19 +0200

    [fix] responsive

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,73 +1,116 @@
 "use client";
 import Link from "next/link";
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 
-export default function Header({ active }: { active: number }) {
-  const [items, setItems] = useState([
-    { name: "Présentation", slug: "presentation" },
-    { name: "Formations", slug: "formations" },
-    { name: "Découvrir", slug: "decouvrir" },
-    { name: "Compétences", slug: "competences" },
-    { name: "Expériences", slug: "experiences" },
-  ]);
+export default function Header({
+  active,
+  items,
+  activeTabRef,
+}: {
+  active: number;
+  items: any;
+  activeTabRef: any;
+}) {
+  const [ulTranslate, setUlTranslate] = useState(0);
+  const containerRef = useRef<any>(null);
+
+  useEffect(() => {
+    // ...
+
+    // Calculate the translation value for the ul element
+    if (activeTabRef.current) {
+      const tabRect = activeTabRef.current.getBoundingClientRect();
+      if (tabRect.right > window.innerWidth) {
+        // Scroll right to bring the tab into view
+        const translateValue = tabRect.right - window.innerWidth;
+        if (containerRef.current) {
+          containerRef.current.scrollLeft = translateValue + 100; // Scroll to the beginning
+        }
+
+        setUlTranslate(translateValue);
+      } else if (tabRect.left < 0) {
+        // Scroll left to bring the tab into view
+        const translateValue = tabRect.left + window.scrollX;
+        if (containerRef.current) {
+          containerRef.current.scrollLeft = translateValue; // Scroll to the beginning
+        }
+      }
+    }
+
+    // ...
+  }, [active, activeTabRef]);
 
   return (
-    <div className="w-full h-16 sticky top-0 z-10 bg-white flex items-center justify-between px-6">
-      <p className="text-xl font-black tracking-wider">SARAH KTORZA</p>
-      <ul className="h-full absolute left-1/2 -translate-x-1/2 flex items-center justify-center gap-4 font-medium">
-        {items.map((item, index) => (
-          <li key={index}>
-            <Link
-              className={`uppercase ${
-                active === index + 1 ? "text-black" : "text-gray-disabled"
-              } hover:text-black duration-150 px-2 py-2`}
-              href={`/#${item.slug}`}
+    <div className="w-full h-16 sticky top-0 z-10 bg-white px-6">
+      <div className="relative w-full h-full flex items-center justify-between">
+        <p className="text-xl font-black tracking-wider text-black z-20">
+          SARAH KTORZA
+        </p>
+        <div
+          ref={containerRef}
+          className="h-10 bg-white overflow-x-auto w-screen px-4 lg:h-full absolute left-1/2  -translate-x-1/2 bottom-0 translate-y-full lg:relative lg:left-0 lg:translate-x-0 xl:absolute xl:left-1/2 xl:-translate-x-1/2 xl:top-0 lg:translate-y-0 flex justify-start sm:justify-center items-center"
+        >
+          <ul
+            id="header-list"
+            className="flex items-center w-fit justify-center gap-4 font-medium"
+          >
+            {items.map((item: any, index: number) => (
+              <li key={index}>
+                <Link
+                  ref={active === index + 1 ? activeTabRef : null}
+                  className={`uppercase ${
+                    active === index + 1 ? "text-black" : "text-gray-disabled"
+                  } hover:text-black duration-150 px-2 py-2`}
+                  href={`/#${item.slug}`}
+                >
+                  {item.name}
+                </Link>
+              </li>
+            ))}
+          </ul>
+        </div>
+
+        <div className="flex items-center gap-6">
+          <a
+            href="https://www.linkedin.com/in/sarah-k-evenementiel/"
+            target="_blank"
+            className="text-black hover:text-primary duration-150"
+          >
+            <svg
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
             >
-              {item.name}
-            </Link>
-          </li>
-        ))}
-      </ul>
-      <div className="flex items-center gap-6">
-        <a
-          href="https://www.linkedin.com/in/sarah-k-evenementiel/"
-          target="_blank"
-          className="text-black hover:text-primary duration-150"
-        >
-          <svg
-            width="24"
-            height="24"
-            viewBox="0 0 24 24"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg"
+              <path
+                d="M8 19H5V9H8V19ZM19 19H16V13.658C16 12.266 15.504 11.573 14.521 11.573C13.742 11.573 13.248 11.961 13 12.738V19H10C10 19 10.04 10 10 9H12.368L12.551 11H12.613C13.228 10 14.211 9.322 15.559 9.322C16.584 9.322 17.413 9.607 18.046 10.323C18.683 11.04 19 12.002 19 13.353V19Z"
+                fill="currentColor"
+              />
+              <path
+                d="M6.50001 8C7.35605 8 8.05001 7.32843 8.05001 6.5C8.05001 5.67157 7.35605 5 6.50001 5C5.64397 5 4.95001 5.67157 4.95001 6.5C4.95001 7.32843 5.64397 8 6.50001 8Z"
+                fill="currentColor"
+              />
+            </svg>
+          </a>
+          <a
+            href="mailto:sarahktorza@gmail.com"
+            className="text-black hover:text-primary duration-150"
           >
-            <path
-              d="M8 19H5V9H8V19ZM19 19H16V13.658C16 12.266 15.504 11.573 14.521 11.573C13.742 11.573 13.248 11.961 13 12.738V19H10C10 19 10.04 10 10 9H12.368L12.551 11H12.613C13.228 10 14.211 9.322 15.559 9.322C16.584 9.322 17.413 9.607 18.046 10.323C18.683 11.04 19 12.002 19 13.353V19Z"
-              fill="currentColor"
-            />
-            <path
-              d="M6.50001 8C7.35605 8 8.05001 7.32843 8.05001 6.5C8.05001 5.67157 7.35605 5 6.50001 5C5.64397 5 4.95001 5.67157 4.95001 6.5C4.95001 7.32843 5.64397 8 6.50001 8Z"
-              fill="currentColor"
-            />
-          </svg>
-        </a>
-        <a
-          href="mailto:sarahktorza@gmail.com"
-          className="text-black hover:text-primary duration-150"
-        >
-          <svg
-            width="24"
-            height="24"
-            viewBox="0 0 24 24"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path
-              d="M21 4.6875H3C2.85082 4.6875 2.70774 4.74676 2.60225 4.85225C2.49676 4.95774 2.4375 5.10082 2.4375 5.25V18C2.4375 18.3481 2.57578 18.6819 2.82192 18.9281C3.06806 19.1742 3.4019 19.3125 3.75 19.3125H20.25C20.5981 19.3125 20.9319 19.1742 21.1781 18.9281C21.4242 18.6819 21.5625 18.3481 21.5625 18V5.25C21.5625 5.10082 21.5032 4.95774 21.3977 4.85225C21.2923 4.74676 21.1492 4.6875 21 4.6875ZM12 12.7369L4.44562 5.8125H19.5544L12 12.7369ZM9.53156 12L3.5625 17.4713V6.52875L9.53156 12ZM10.3641 12.7631L11.625 13.9144C11.7287 14.0092 11.8641 14.0619 12.0047 14.0619C12.1452 14.0619 12.2807 14.0092 12.3844 13.9144L13.6406 12.7631L19.5544 18.1875H4.44656L10.3641 12.7631ZM14.4684 12L20.4375 6.52875V17.4713L14.4684 12Z"
-              fill="currentColor"
-            />
-          </svg>
-        </a>
+            <svg
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                d="M21 4.6875H3C2.85082 4.6875 2.70774 4.74676 2.60225 4.85225C2.49676 4.95774 2.4375 5.10082 2.4375 5.25V18C2.4375 18.3481 2.57578 18.6819 2.82192 18.9281C3.06806 19.1742 3.4019 19.3125 3.75 19.3125H20.25C20.5981 19.3125 20.9319 19.1742 21.1781 18.9281C21.4242 18.6819 21.5625 18.3481 21.5625 18V5.25C21.5625 5.10082 21.5032 4.95774 21.3977 4.85225C21.2923 4.74676 21.1492 4.6875 21 4.6875ZM12 12.7369L4.44562 5.8125H19.5544L12 12.7369ZM9.53156 12L3.5625 17.4713V6.52875L9.53156 12ZM10.3641 12.7631L11.625 13.9144C11.7287 14.0092 11.8641 14.0619 12.0047 14.0619C12.1452 14.0619 12.2807 14.0092 12.3844 13.9144L13.6406 12.7631L19.5544 18.1875H4.44656L10.3641 12.7631ZM14.4684 12L20.4375 6.52875V17.4713L14.4684 12Z"
+                fill="currentColor"
+              />
+            </svg>
+          </a>
+        </div>
       </div>
     </div>
   );
--- app/page.tsx
+++ app/page.tsx
@@ -7,33 +7,52 @@ import Skills from "./components/Skills";
 import Experiences from "./components/Experiences";
 import Frise from "./components/Frise";
 import Header from "./components/Header";
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 
 export default function Home() {
   const [activeTab, setActiveTab] = useState<number>(1);
+  const items: any = [
+    { name: "Présentation", slug: "presentation" },
+    { name: "Formations", slug: "formations" },
+    { name: "Découvrir", slug: "decouvrir" },
+    { name: "Compétences", slug: "competences" },
+    { name: "Expériences", slug: "experiences" },
+  ];
+
+  const activeTabRef = useRef(null);
 
   useEffect(() => {
-    // Function to check if a section is in the viewport
     const isSectionInView = (sectionId: string) => {
       const element = document.getElementById(sectionId);
       if (!element) return false;
 
       const rect = element.getBoundingClientRect();
-      return rect.top >= 0 && rect.bottom <= window.innerHeight;
+
+      // Check if the element's top or bottom is within the viewport
+      return (
+        (rect.top >= 0 && rect.top <= window.innerHeight) ||
+        (rect.bottom >= 0 && rect.bottom <= window.innerHeight)
+      );
     };
 
     // Function to handle scroll events
     const handleScroll = () => {
+      // Update the active tab as before
       if (isSectionInView("presentation")) {
         setActiveTab(1);
+        console.log("presentation");
       } else if (isSectionInView("formations")) {
         setActiveTab(2);
+        console.log("formations");
       } else if (isSectionInView("decouvrir")) {
         setActiveTab(3);
+        console.log("decouvrir");
       } else if (isSectionInView("competences")) {
         setActiveTab(4);
+        console.log("competences");
       } else if (isSectionInView("experiences")) {
         setActiveTab(5);
+        console.log("experiences");
       }
     };
 
@@ -42,21 +61,23 @@ export default function Home() {
 
     document.addEventListener("DOMContentLoaded", function () {
       const links = document.querySelectorAll("a[href^='#']");
-  
+
       links.forEach((link: any) => {
         link.addEventListener("click", (e: any) => {
           e.preventDefault();
-  
+
           const targetId = link.getAttribute("href").substring(1); // Get the target section's ID
           const targetSection = document.getElementById(targetId);
-  
+
           if (targetSection) {
             const offset = 50; // Adjust the offset as needed
-  
+
             // Calculate the scroll position
             const scrollPosition =
-              targetSection.getBoundingClientRect().top + window.scrollY + offset;
-  
+              targetSection.getBoundingClientRect().top +
+              window.scrollY +
+              offset;
+
             window.scrollTo({
               top: scrollPosition,
               behavior: "smooth",
@@ -71,10 +92,13 @@ export default function Home() {
     };
   }, []);
 
-
   return (
     <>
-      <Header active={activeTab} />
+      <Header
+        active={activeTab}
+        items={items}
+        activeTabRef={activeTabRef}
+      />
       <Hero />
       <Frise />
       <Discover />
