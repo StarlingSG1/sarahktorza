341c817e47d376881d459fa24a72c130c5c24f35
Jérémie <barriere.jeremie@gmail.com>
2024-04-03 11:59:55 +0200

    [update] change primary color

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,7 +13,7 @@ module.exports = {
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
       colors: {
-        primary: '#e5c5c2',
+        primary: '#EBC4C2',
         gray: {
           "disabled" : "#BABABA"
         },
